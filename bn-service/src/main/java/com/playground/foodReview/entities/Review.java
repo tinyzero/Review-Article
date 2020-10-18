c7d191467262e99e7a2323c13d2ac4d69b3d394d
Tanakorn (Nine) Tragoonrungsi <tanakorn.tra@truedigital.com>
2020-10-18 17:45:02 +0700

    update keyword-review-mapping

--- bn-service/src/main/java/com/playground/foodReview/entities/Review.java
+++ bn-service/src/main/java/com/playground/foodReview/entities/Review.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -12,6 +13,7 @@ import java.util.Set;
 @Data
 public class Review implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     @JsonProperty("id")
     private Integer id;
--- bn-service/src/main/java/com/playground/foodReview/repositories/ReviewKeywordMappingRepository.java
+++ bn-service/src/main/java/com/playground/foodReview/repositories/ReviewKeywordMappingRepository.java
@@ -2,8 +2,24 @@ package com.playground.foodReview.repositories;
 
 import com.playground.foodReview.entities.ReviewKeywordMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ReviewKeywordMappingRepository extends JpaRepository<ReviewKeywordMapping,Integer> {
+
+    @Query(value = "SELECT keyword_id, review_id FROM review_keyword_mapping WHERE review_id = ?1", nativeQuery = true)
+    public List<ReviewKeywordMapping> findByReview_id(Integer review_id);
+
+    @Transactional
+    @Modifying
+    @Query(
+            value = "INSERT INTO review_keyword_mapping (review_id, keyword_id) SELECT ?1 as review_id, kt.id as keyword_id FROM keyword kt WHERE ?2 like concat('%',kt.keyword,'%')",
+            nativeQuery = true)
+    public void insertMapping(Integer review_id, String review);
+
 }
